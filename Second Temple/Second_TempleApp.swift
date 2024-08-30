d7d2e35cc593794f719497dc482dfc7173aa2efc
ShlomoCode <78599753+ShlomoCode@users.noreply.github.com>
2024-08-30 13:02:54 +0300

    Initial Commit

---
+++ Second Temple/Second_TempleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Second_TempleApp.swift
+//  Second Temple
+//
+//  Created by shlomo on 30/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Second_TempleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
